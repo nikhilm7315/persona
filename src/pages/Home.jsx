24632c24901b7f925a629ed991b604f5a459f53b
nikhim7315 <nikhilmishra7315@gmail.com>
2026-07-05 01:56:09 +0530

    intial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-gray-800">
+        Welcome to Persona 🎓
+      </h1>
+    </main>
+  )
+}
+
+export default Home
